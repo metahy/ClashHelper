5135886b6a25f168a5f5149af1ff1217249858e1
hyleon <hyleontsao@gmail.com>
2022-01-20 12:01:31 +0800

    refactor: add ConfigUtil

--- src/main/java/io/metahy/App.java
+++ src/main/java/io/metahy/App.java
@@ -6,62 +6,18 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import org.apache.commons.io.FileUtils;
-import org.ini4j.Wini;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Objects;
 
+/**
+ * @author Hyleon
+ */
 public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        final String userHome = System.getProperty("user.home");
-        final String userHomeCfgDir = userHome + File.separator + ".config";
-        final String targetDir = userHomeCfgDir + File.separator + "clash_helper";
-        final String targetFile = targetDir + File.separator + "config.ini";
-        final String defaultClashDir = userHomeCfgDir + File.separator + "clash";
-
-        System.out.println("");
-        System.out.println("===== System =====");
-        System.out.println(" => OS: " + System.getProperty("os.name"));
-        System.out.println(" => Uh: " + userHome);
-        System.out.println("==================");
-        System.out.println("");
-
-        final File td = FileUtils.getFile(targetDir);
-        if (td.exists() && td.isFile()) {
-            System.err.println("ERROR-0: Target config directory is file, please remove " + targetDir);
-            return;
-        }
-
-        if (!td.exists()) {
-            System.out.println("INFO: Create config directory:[" + targetDir + "], result:[" + td.mkdirs() + "]");
-        }
-
-        final File tf = FileUtils.getFile(targetFile);
-        if (!tf.exists()) {
-            System.out.println("INFO: Create config file:[" + targetFile + "], result:[" + tf.createNewFile() + "]");
-        }
-
-        Wini ini = new Wini(new FileInputStream(tf));
-        String currCd = ini.get("Clash", "config_dir", String.class);
-
-        System.out.println("INFO: Current Clash config directory:[" + currCd + "]");
-
-        if (Objects.isNull(currCd) || currCd.length() == 0) {
-            System.out.println("INFO: Use default Clash config directory:[" + defaultClashDir + "]");
-            currCd = defaultClashDir;
-        }
-
-        ini.put("Clash", "config_dir", defaultClashDir);
-        ini.store(new FileOutputStream(tf));
-
-        MainController.DIR = currCd;
-
+        ConfigUtil.prepare();
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/main.fxml")));
         stage.setTitle("Clash Helper");
         stage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResource("/logo.png")).openStream()));
---
+++ src/main/java/io/metahy/ConfigUtil.java
@@ -0,0 +1,71 @@
+package io.metahy;
+
+import org.apache.commons.io.FileUtils;
+import org.ini4j.Wini;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * @author Hyleon
+ */
+public class ConfigUtil {
+
+    private static Wini ini = null;
+    private static final String USER_HOME = System.getProperty("user.home");
+    private static final String USER_HOME_CFG_DIR = USER_HOME + File.separator + ".config";
+    private static final String TARGET_DIR = USER_HOME_CFG_DIR + File.separator + "clash_helper";
+    private static final String TARGET_FILE = TARGET_DIR + File.separator + "config.ini";
+    private static final String DEFAULT_CLASH_DIR = USER_HOME_CFG_DIR + File.separator + "clash";
+
+    public static void prepare() throws IOException {
+        logSys();
+
+        final File td = FileUtils.getFile(TARGET_DIR);
+        if (td.exists() && td.isFile()) {
+            System.err.println("ERROR-0: Target config directory is file, please remove " + TARGET_DIR);
+            return;
+        }
+
+        if (!td.exists()) {
+            System.out.println("INFO: Create config directory:[" + TARGET_DIR + "], result:[" + td.mkdirs() + "]");
+        }
+
+        final File tf = FileUtils.getFile(TARGET_FILE);
+        if (!tf.exists()) {
+            System.out.println("INFO: Create config file:[" + TARGET_FILE + "], result:[" + tf.createNewFile() + "]");
+        }
+
+        ini = new Wini(new FileInputStream(tf));
+        String currCd = ini.get("Clash", "config_dir", String.class);
+
+        System.out.println("INFO: Current Clash config directory:[" + currCd + "]");
+
+        if (Objects.isNull(currCd) || currCd.length() == 0) {
+            System.out.println("INFO: Use default Clash config directory:[" + DEFAULT_CLASH_DIR + "]");
+            currCd = DEFAULT_CLASH_DIR;
+        }
+
+        ini.put("Clash", "config_dir", DEFAULT_CLASH_DIR);
+        ini.store(new FileOutputStream(tf));
+
+        MainController.DIR = currCd;
+    }
+
+    public static void setClashDir(String dir) throws IOException {
+        ini.put("Clash", "config_dir", dir);
+        ini.store(new FileOutputStream(FileUtils.getFile(TARGET_FILE)));
+    }
+
+    private static void logSys() {
+        System.out.println();
+        System.out.println("===== System =====");
+        System.out.println(" => OS: " + System.getProperty("os.name"));
+        System.out.println(" => Uh: " + System.getProperty("user.home"));
+        System.out.println("==================");
+        System.out.println();
+    }
+}
